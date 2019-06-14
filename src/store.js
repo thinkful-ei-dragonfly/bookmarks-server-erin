b85569fa0c05d711f55c962d2e641d72fa6efc00
Erin Dustin <erincdustin@gmail.com>
2019-06-14 14:37:51 -0500

    GET, POST, DELETE routes working

---
+++ src/store.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const bookmarks = [
+  {
+    title: 'Something',
+    url: 'https://courses.thinkful.com/ei-node-postgres-v1/checkpoint/9',
+    desc: 'This is a description',
+    rating: 4,
+  }
+];
+
+module.exports = {bookmarks};
